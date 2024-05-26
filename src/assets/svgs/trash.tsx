bab2a709e0abf1cc0320a2df4516fc95762879b4
fiona-byte <fionaodoboh@gmail.com>
2024-05-26 09:49:02 +0100

    chore: add svg icons

---
+++ src/assets/svgs/trash.tsx
@@ -0,0 +1,50 @@
+const Trash = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      className={className}
+      width="25"
+      height="24"
+      viewBox="0 0 25 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M21.5 5.98001C18.17 5.65001 14.82 5.48001 11.48 5.48001C9.5 5.48001 7.52 5.58001 5.54 5.78001L3.5 5.98001"
+        stroke="#3A3235"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9 4.97L9.22 3.66C9.38 2.71 9.5 2 11.19 2H13.81C15.5 2 15.63 2.75 15.78 3.67L16 4.97"
+        stroke="#3A3235"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M19.35 9.14001L18.7 19.21C18.59 20.78 18.5 22 15.71 22H9.29C6.5 22 6.41 20.78 6.3 19.21L5.65 9.14001"
+        stroke="#3A3235"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.83 16.5H14.16"
+        stroke="#3A3235"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10 12.5H15"
+        stroke="#3A3235"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Trash;
