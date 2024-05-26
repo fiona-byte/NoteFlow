78e27a888bbe99af82de086c1c5a7aa065c2878d
fiona-byte <fionaodoboh@gmail.com>
2024-05-26 20:23:13 +0100

    fix: updates

--- src/pages/NotesArchive/NotesArchive.tsx
+++ src/pages/NotesArchive/NotesArchive.tsx
@@ -1,7 +1,6 @@
 import { useMobile } from "@/hooks/useMobile";
 import DesktopNavigation from "@/components/DesktopNavigation/DesktopNavigation";
 import MobileNavigation from "@/components/MobileNavigation/MobileNavigation";
-import Card from "@/components/Card/Card";
 
 function NotesArchive() {
   const isMobile = useMobile();
@@ -9,9 +8,6 @@ function NotesArchive() {
   return (
     <div className="bg-main h-screen">
       {isMobile ? <MobileNavigation /> : <DesktopNavigation />}
-      <div>
-        <Card />
-      </div>
     </div>
   );
 }
