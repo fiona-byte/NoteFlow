2a5e46d1dda5d06778fb12c40861686506921900
fiona-byte <fionaodoboh@gmail.com>
2024-06-10 12:05:13 +0100

    fix: ui bugs

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,22 +13,22 @@ function Header() {
       <div className="relative md:w-1/2 lg:w-1/3">
         <Search className="absolute left-4 top-4 h-4 w-4" />
         <Input
-          className="pr-5 pl-10 py-6 rounded-[10px] text-base"
+          className="pr-5 pl-10 py-6 rounded-xl text-base"
           type="search"
           placeholder="Search notes"
         />
       </div>
-      <div className="flex md:gap-6 lg:gap-8">
+      <div className="flex md:gap-4">
         <Button
           variant="outline"
-          className="px-5 py-6 rounded-[10px] hover:bg-transparent"
+          className="px-5 py-6 rounded-xl hover:bg-transparent"
         >
           <ListFilterIcon />
         </Button>
         {!isMobile ? (
           <Button
             onClick={() => navigate("/create")}
-            className="text-[19px] font-normal px-5 py-6 border border-textColor hover:bg-textColor rounded-[10px]"
+            className="text-[19px] font-normal px-5 py-6 border border-textColor hover:bg-textColor rounded-xl"
           >
             Add
           </Button>
