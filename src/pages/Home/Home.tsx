1685c099f44eb60b74c3515b25fc3bccec09d5b4
fiona-byte <fionaodoboh@gmail.com>
2024-06-12 21:46:27 +0100

    fix: design bugs

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -19,7 +19,7 @@ function Home() {
       ) : (
         <>
           {favourites.length ? (
-            <div className="mb-16">
+            <div className="mb-12">
               <div className="flex justify-between items-center">
                 <h2 className="font-medium text-[28px] lg:text-[33px]">
                   Favourites
@@ -27,13 +27,13 @@ function Home() {
                 {favourites.length >= 4 ? (
                   <Link
                     to="/favourites"
-                    className="relative before:content-[''] before:block before:h-[1.5px] before:w-full before:bg-border before:rounded-sm before:absolute before:bottom-0 before:left-0 lg:before:origin-right lg:before:scale-x-0 lg:before:transition-transform hover:lg:before:origin-left hover:lg:before:scale-x-100 lg:text-lg"
+                    className="relative before:content-[''] before:block before:h-1.5 before:w-full before:bg-border before:rounded-sm before:absolute before:bottom-0 before:left-0 lg:before:origin-right lg:before:scale-x-0 lg:before:transition-transform hover:lg:before:origin-left hover:lg:before:scale-x-100 lg:text-lg"
                   >
                     View all
                   </Link>
                 ) : null}
               </div>
-              <div className="grid grid-cols-2 gap-5 mt-6 md:grid-cols-3 lg:grid-cols-4 md:gap-y-8">
+              <div className="grid grid-cols-2 gap-5 mt-4 md:grid-cols-3 lg:grid-cols-4 md:gap-y-8">
                 {favourites.slice(0, 4).map((note) => (
                   <Card note={note} key={note.id} />
                 ))}
@@ -47,13 +47,13 @@ function Home() {
             {notes.length >= 4 ? (
               <Link
                 to="/notes"
-                className="relative before:content-[''] before:block before:h-[1.5px] before:w-full before:bg-border before:rounded-sm before:absolute before:bottom-0 before:left-0 lg:before:origin-right lg:before:scale-x-0 lg:before:transition-transform hover:lg:before:origin-left hover:lg:before:scale-x-100 lg:text-lg"
+                className="relative before:content-[''] before:block before:h-1.5 before:w-full before:bg-border before:rounded-sm before:absolute before:bottom-0 before:left-0 lg:before:origin-right lg:before:scale-x-0 lg:before:transition-transform hover:lg:before:origin-left hover:lg:before:scale-x-100 lg:text-lg"
               >
                 View all
               </Link>
             ) : null}
           </div>
-          <div className="grid grid-cols-2 gap-5 mt-6 md:grid-cols-3 lg:grid-cols-4 md:gap-y-8">
+          <div className="grid grid-cols-2 gap-5 mt-4 md:grid-cols-3 lg:grid-cols-4 md:gap-y-8">
             {notes.slice(0, 4).map((note) => (
               <Card note={note} key={note.id} />
             ))}
