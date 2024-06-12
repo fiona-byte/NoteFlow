034b881745153320987cf8ce27fd77984bc40796
fiona-byte <fionaodoboh@gmail.com>
2024-06-12 21:33:32 +0100

    fix: design bugs

--- src/layouts/AppLayout/index.tsx
+++ src/layouts/AppLayout/index.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { useMobile } from "@/hooks/useMobile";
 import DesktopSidebar from "@/components/DesktopSidebar/DesktopSidebar";
-import MobileSidebar from "@/components/MobileSidebar/MobileSidebar";
+import MobileNavigation from "@/components/MobileNavigation/MobileNavigation";
 import Header from "@/components/Header/Header";
 
 function AppLayout() {
@@ -9,8 +9,8 @@ function AppLayout() {
 
   return (
     <div className="bg-main h-screen md:flex xl:max-w-7xl xl:mx-auto">
-      {isMobile ? <MobileSidebar /> : <DesktopSidebar />}
-      <div className="h-screen overflow-auto px-4 py-8 md:px-10 md:flex-1 lg:px-20 lg:py-10">
+      {isMobile ? <MobileNavigation /> : <DesktopSidebar />}
+      <div className="h-screen overflow-auto px-4 pt-8 pb-32 md:px-10 md:flex-1 lg:px-20 lg:py-10">
         <Header />
         <Outlet />
       </div>
