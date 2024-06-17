1be3ada5c0465da7bf911d76c702f19780ac3edc
fiona-byte <fionaodoboh@gmail.com>
2024-06-17 23:09:50 +0100

    chore: remove unused component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 import Home from "./pages/Home/Home";
 import NotesArchive from "./pages/NotesArchive/NotesArchive";
-import CreateNote from "./pages/CreateNote/CreateNote";
 import SingleNote from "./pages/SingleNote/SingleNote";
 import Trash from "./pages/Trash/Trash";
 import Favourite from "./pages/Favourites/Favourites";
@@ -14,7 +13,6 @@ function App() {
         <Route path="/" element={<AppLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/notes" element={<NotesArchive />} />
-          <Route path="/create" element={<CreateNote />} />
           <Route path="/edit/:id" element={<SingleNote />} />
           <Route path="/favourites" element={<Favourite />} />
           <Route path="/trash" element={<Trash />} />
