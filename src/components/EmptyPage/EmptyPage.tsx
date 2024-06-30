084e9f24c3024b8c3de1e31f8de7c78d5573e48e
fiona-byte <fionaodoboh@gmail.com>
2024-06-30 09:35:54 +0100

    fix: update pathname condition

--- src/components/EmptyPage/EmptyPage.tsx
+++ src/components/EmptyPage/EmptyPage.tsx
@@ -11,6 +11,9 @@ function EmptyPage({
   const location = useLocation();
   const addNote = useCreateNote();
 
+  const pathnameCondition =
+    location.pathname === "/" || location.pathname === "/notes";
+
   return (
     <div className="flex justify-center flex-col items-center p-4 w-full absolute top-[42%] left-2/4 translate-x-[-50%] translate-y-[-50%] lg:top-[44%]">
       <PageIllustration className="w-28 h-28 lg:w-32 lg:h-32" />
@@ -21,7 +24,7 @@ function EmptyPage({
         <p className="text-center md:text-[17px] lg:text-[19px]">
           {pageSubTitle}
         </p>
-        {location.pathname === "/" ? (
+        {pathnameCondition ? (
           <Button
             onClick={() => addNote()}
             className="px-6 py-6 mt-6 font-normal text-[17px] mx-auto flex lg:text-[19px] hover:bg-textColor"
