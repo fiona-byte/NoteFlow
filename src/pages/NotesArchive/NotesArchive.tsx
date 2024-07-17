311d1dcfe6a73572fea3a9124e09fc51428135c1
fiona-byte <fionaodoboh@gmail.com>
2024-07-17 23:05:57 +0100

    chore: add empty filter screen

--- src/pages/NotesArchive/NotesArchive.tsx
+++ src/pages/NotesArchive/NotesArchive.tsx
@@ -1,8 +1,9 @@
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
 import NoNotesIllustration from "@/assets/svgs/noNotes";
 import Card from "@/components/Card/Card";
 import EmptyPage from "@/components/EmptyPage/EmptyPage";
-import { useSelector } from "react-redux";
-import { RootState } from "@/redux/store";
+import NoFilterResultsIllustration from "@/assets/svgs/noFilterResults";
 
 function NotesArchive() {
   const { notes, tags, selectedTags } = useSelector(
@@ -26,16 +27,32 @@ function NotesArchive() {
         />
       ) : (
         <>
-          <h2 className="font-medium text-[28px] lg:text-[33px]">
-            {selectedTags.length > 0
-              ? `Results (${filteredNotes.length})`
-              : "All Notes"}
-          </h2>
-          <div className="grid grid-cols-2 gap-5 mt-4 md:grid-cols-3 md:gap-y-8 lg:grid-cols-4">
-            {filteredNotes?.map((note) => (
-              <Card note={note} tags={tags} key={note.id} />
-            ))}
-          </div>
+          {selectedTags.length > 0 ? (
+            <>
+              <h2 className="font-medium text-[28px] lg:text-[33px]">
+                {selectedTags.length > 0
+                  ? `Results (${filteredNotes.length})`
+                  : "All Notes"}
+              </h2>
+              <div className="grid grid-cols-2 gap-5 mt-4 md:grid-cols-3 md:gap-y-8 lg:grid-cols-4">
+                {filteredNotes?.map((note) => (
+                  <Card note={note} tags={tags} key={note.id} />
+                ))}
+              </div>
+            </>
+          ) : (
+            <div className="flex justify-center flex-col items-center p-4 w-full absolute top-[42%] left-2/4 translate-x-[-50%] translate-y-[-50%] lg:top-[44%]">
+              <NoFilterResultsIllustration className="w-28 h-28 lg:w-32 lg:h-32" />
+              <div className="mt-6">
+                <h2 className="text-[28px] font-medium mb-2 text-center">
+                  No Results{" "}
+                </h2>
+                <p className="text-center md:text-[17px] lg:text-[19px]">
+                  There are no notes for this tag.
+                </p>
+              </div>
+            </div>
+          )}
         </>
       )}
     </div>
